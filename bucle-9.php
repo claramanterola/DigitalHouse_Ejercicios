aed390964dc36de948f9d8a3a22730fd42436acc
Clara <39087696+claramanterola@users.noreply.github.com>
2019-09-02 22:10:02 -0300

    Add files via upload

---
+++ bucle-9.php
@@ -0,0 +1,37 @@
+<?php
+/*
+9. Partiendo de un archivo con la siguiente variable definida:
+
+$ceu = array(
+"Italia"=>"Roma",
+"Luxembourg"=>"Luxembourg",
+"Bélgica"=> "Bruselas",
+"Dinamarca"=>"Copenhagen",
+"Finlandia"=>"Helsinki",
+"Francia" => "Paris",
+"Slovakia"=>"Bratislava",
+"Eslovenia"=>"Ljubljana",
+"Alemania" => "Berlin",
+"Grecia" => "Athenas",
+"Irlanda"=>"Dublin",
+"Holanda"=>"Amsterdam",
+"Portugal"=>"Lisbon",
+"España"=>"Madrid",
+"Suecia"=>"Stockholm",
+"Reino Unido"=>"London",
+"Chipre"=>"Nicosia",
+"Lithuania"=>"Vilnius",
+"Republica Checa"=>"Prague",
+"Estonia"=>"Tallin",
+"Hungría"=>"Budapest",
+"Latvia"=>"Riga",
+"Malta"=>"Valletta",
+"Austria" => "Vienna",
+"Polonia"=>"Warsaw") ;
+
+Crear un script que muestre el nombre de la capital
+y el país desde la variable $ceu.
+*/
+
+
+ ?>
